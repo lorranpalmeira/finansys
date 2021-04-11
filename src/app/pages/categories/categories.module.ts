cc44b38ee1a32023d8d4197e3f43b4182e72d8cf
lorranpalmeira <lorranpalmeira@outlook.com>
2021-04-10 21:58:05 -0300

    improving category list

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryService } from '../shared/category.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    
   ],
   declarations: [CategoryListComponent, CategoryFormComponent]
 })
